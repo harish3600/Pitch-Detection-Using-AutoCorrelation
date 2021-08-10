8bf0c2c01c5ddf4d6cf4906cd85f1e7ae5dac306
Harish Umasankar <71188815+harish3600@users.noreply.github.com>
2021-08-10 09:41:51 +0400

    Add files via upload

---
+++ recordSpeech.m
@@ -0,0 +1,17 @@
+clear; clc;
+
+fs = 10000;
+
+recorder = audiorecorder(fs, 8,1);
+recordblocking(recorder,2); %recorder turned ON for 2 seconds
+
+play(recorder)
+
+%y - Array to store the recorded data
+y = getaudiodata(recorder);
+filename = 'speech2.wav';
+
+%Write data in y to .wav file
+audiowrite('speech2.wav', y, 10000);
+info = audioinfo(filename);
+disp(info);
